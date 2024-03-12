540646d1aa67e121378005af5c1db7a9ec63b92f
Sahil <161432826+sahilnandivkar@users.noreply.github.com>
2024-03-12 17:52:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+$(window).scroll(function(){
+    if(this.scrollY > 20){
+        $('.navbar').addClass("sticky");
+    }else{
+        $('.navbar').removeClass("sticky");
+    }
+
+    if(this.scrollY > 500){
+        $('.scroll-up-btn').addClass("show");
+    }else{
+        $('.scroll-up-btn').removeClass("show");
+    }
+});
